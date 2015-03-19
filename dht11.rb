669572c853e7ac273f60941f23fa9ab5a0051ad5
tacyan <sigotoyou2014@yahoo.co.jp>
2015-03-19 21:07:23 +0900

    add redis

--- dht11.rb
+++ dht11.rb
@@ -1,6 +1,7 @@
 #cding: utf-8
 require 'sinatra'
 require 'json'
+require 'redis'
 
 set :environment, :production
 set :port, 1234
@@ -8,14 +9,29 @@ set :port, 1234
 
   get '/' do
 
+    redis = Redis.new host:"127.0.0.1", port:"6379"
+    @temperature = redis.get("temperature") 
     erb :index
   end
 
-  get '/:temperature' do
-    @temperature = "[#{params[:temperature]}]"
-    
-    erb :index
+  get '/test/:temperature' do
+    redis = Redis.new host:"127.0.0.1", port:"6379"
+    redis.set("temperature", "[#{params[:temperature]}]")
+     
+    redis.get("temperature") 
   end
+
+  get '/temperature' do
+    redis = Redis.new host:"127.0.0.1", port:"6379"
+    @temperature = redis.get("temperature") 
+    
+  end
+
+  #get '/:temperature' do
+  #  @temperature = "[#{params[:temperature]}]"
+  #  
+  #  erb :index
+  #end
 
   post '/', provides: :json do
     @temp = JSON.parse request.body.read
